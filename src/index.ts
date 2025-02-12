5e6b84d4a1fa40520bc779039633d466d984cff0
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-12 15:39:58 +0530

    SQL injection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import {Client} from "pg";
+import { Client } from "pg";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-const pgClient = new Client("postgresql://test_owner:CoBLVJb5Oh0e@ep-mute-bread-a5ufmcdh-pooler.us-east-2.aws.neon.tech/test?sslmode=require")
+const pgClient = new Client(
+    "postgresql://test_owner:CoBLVJb5Oh0e@ep-mute-bread-a5ufmcdh-pooler.us-east-2.aws.neon.tech/test?sslmode=require"
+);
+//This url also should be hidden in a seperate env file
 
-async function main(){
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users;")
-    console.log(response);
-}
+pgClient.connect();
 
-main();
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    // const response = await pgClient.query(`INSERT INTO users (username, email, password) values ('${username}', '${email}', '${password}')`);
+    //this will be prone to sql injection
+
+    //rather:
+
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`;
+
+        const response = await pgClient.query(insertQuery, [
+            username,
+            email,
+            password,
+        ]);
+
+        res.json({
+            message: "You have signed up",
+        });
+    } catch (e) {
+        console.log(e);
+        res.json({
+            message: "You have signed up",
+        });
+    }
+});
+
+app.listen(3000);
